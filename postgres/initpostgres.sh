80e195f038b4ee8f2d934266c78d14f08cdc1721
codemarc <codemarc@gmail.com>
2015-03-17 16:38:04 -0400

    populate the environment

--- postgres/initpostgres.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/ash
-docker run -it  --link postgres:postgres --rm postgres:9.4 sh -c 'exec psql -h postgres -p 5432 -U postgres << EOF
-CREATE DATABASE omni;
-CREATE SCHEMA healthviews;
-EOF
-'
