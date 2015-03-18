6ad5b32bb943b8eba359bb68c611c1de665c55a3
codemarc <codemarc@gmail.com>
2015-03-18 15:53:19 -0400

    wait a sec

--- postgres/init.sh
+++ postgres/init.sh
@@ -4,6 +4,8 @@ echo "CREATE SCHEMA healthviews;" >> init.sql
 
 cat postgres/postgre.hv_1.1.1_fk_1.0.0.0.sql >> init.sql
 
+sleep 1
+
 docker run -it  --link postgres:postgres --rm -v $(pwd):/psql postgres:9.4 \
 sh -c 'exec psql -h postgres -p 5432 -U postgres -f /psql/init.sql' 
 
